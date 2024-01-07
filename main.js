c58e0d8f9bcecb56395ffc3272613fcd65dd7340
Ahsan <sbaseer25@gmail.com>
2024-01-07 16:47:50 -0500

    added a perm toggle

--- main.js
+++ main.js
@@ -54,16 +54,12 @@ var typed1 = new Typed(".text1", {
     "Artificial Intelligence.",
     "Machine Learning.",
     "SaaS.",
-    "Object Oriented Programming.", 
+    "Object Oriented Programming.",
     "RESTful APIs.",
     "IOS.",
     "Android.",
     "HTTP.",
     "Xcode.",
-
-
-    
-
   ],
   typeSpeed: 100,
   backSpeed: 70,
@@ -72,59 +68,78 @@ var typed1 = new Typed(".text1", {
 });
 
 var typed2 = new Typed(".text2", {
-  strings: [
-    "Ahsan.",
-  ],
+  strings: ["Ahsan."],
   typeSpeed: 400,
   backSpeed: 100,
   backDelay: 3000,
   loop: false,
 });
 
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
-window.addEventListener('scroll', function () {
-    header.classList.toggle("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
 };
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('open');
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("open");
 };
 
-document.getElementById('email').addEventListener('click', function() {
-  window.location.href = 'mailto:sbaseer25@gmail.com';
+document.getElementById("email").addEventListener("click", function () {
+  window.location.href = "mailto:sbaseer25@gmail.com";
 });
 
-document.getElementById('email1').addEventListener('click', function() {
-  window.location.href = 'mailto:sbaseer25@gmail.com';
+document.getElementById("email1").addEventListener("click", function () {
+  window.location.href = "mailto:sbaseer25@gmail.com";
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const body = document.body;
-  const themeToggleBtn = document.getElementById('themeToggle');
+  const themeToggleBtn = document.getElementById("themeToggle");
 
   // Check if the user has a preferred theme stored in localStorage
-  const preferredTheme = localStorage.getItem('theme');
+  const preferredTheme = localStorage.getItem("theme");
 
   // Apply the preferred theme or default to 'dark-mode'
-  body.classList.toggle('light-mode', preferredTheme === 'light');
+  body.classList.toggle("light-mode", preferredTheme === "light");
 
   // Add an event listener to the button for theme toggling
-  themeToggleBtn.addEventListener('click', function () {
+  themeToggleBtn.addEventListener("click", function () {
     // Toggle the 'light-mode' class on the body
-    body.classList.toggle('light-mode');
+    body.classList.toggle("light-mode");
 
     // Save the user's preference to localStorage
-    const isLightMode = body.classList.contains('light-mode');
-    localStorage.setItem('theme', isLightMode ? 'light' : 'dark');
+    const isLightMode = body.classList.contains("light-mode");
+    localStorage.setItem("theme", isLightMode ? "light" : "dark");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.body;
+  const themeToggleBtn = document.getElementById("themeToggle1");
+
+  // Check if the user has a preferred theme stored in localStorage
+  const preferredTheme = localStorage.getItem("theme");
+
+  // Apply the preferred theme or default to 'dark-mode'
+  body.classList.toggle("light-mode", preferredTheme === "light");
+
+  // Add an event listener to the button for theme toggling
+  themeToggleBtn.addEventListener("click", function () {
+    // Toggle the 'light-mode' class on the body
+    body.classList.toggle("light-mode");
+
+    // Save the user's preference to localStorage
+    const isLightMode = body.classList.contains("light-mode");
+    localStorage.setItem("theme", isLightMode ? "light" : "dark");
   });
 });
